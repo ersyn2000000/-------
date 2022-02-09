832935462108cc1311a9a7cb557200723021f2a3
ersyn2000000 <geralt606@gmail.com>
2022-02-09 13:10:49 +0600

    first commit

---
+++ password.js
@@ -0,0 +1,12 @@
+function vallidpass() {
+    
+    let password=document.getElementById('password');
+    if (password.value.length==8) {
+        alert('correct')
+        
+    } else {
+        alert('incorect')
+        
+    }
+    
+}
